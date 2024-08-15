a89a95fa663aceea8e2d568b5e1dfc2dfc2e063f
FlavorlessQuark <flavorlessquark@gmail.com>
2024-08-14 17:59:10 -0700

    Resolves #4 | Added tf to backend + example action to create model

---
+++ app/convex/tensorflow_fn.ts
@@ -0,0 +1,18 @@
+"use node"
+import { internalAction, mutation, action } from "./_generated/server";
+import tf from "@tensorflow/tfjs"
+import { build_model } from "./tensorflow/tf_model";
+
+const models: Array<tf.LayersModel> = []
+
+export const _createModelAction = action({
+  args: {},
+  handler: async () => {
+    const model:tf.LayersModel = build_model()
+    models.push(model)
+    // do something with data\
+    console.log("Total models created", models.length)
+    return models.length;
+  },
+});
+
