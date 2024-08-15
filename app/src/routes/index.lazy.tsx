07082e84883f028dd5f827ce0257680e3036d4b8
Bobby Alv <metzgr@gmail.com>
2024-08-15 08:51:07 -0500

    update index

--- app/src/routes/index.lazy.tsx
+++ app/src/routes/index.lazy.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { createLazyFileRoute } from '@tanstack/react-router'
-import {Authenticated, Unauthenticated, useQuery} from "convex/react";
+import { Authenticated, Unauthenticated, useAction, useQuery } from "convex/react";
 import {UserMenu} from "@/components/UserMenu.tsx";
 import {SignInForm} from "@/SignInForm.tsx";
 import {Layout} from "@/Layout.tsx";
@@ -11,6 +12,17 @@ export const Route: any = createLazyFileRoute('/')({
 
 function Index() {
   const user = useQuery(api.users.viewer);
+  const addModelAction = useAction(api.tensorflow_fn._createModelAction);
+  const [modelCount, setCount] = useState<number>(0);
+
+  const doAction = async () => {
+    const res: number = await addModelAction({});
+    setCount(res);
+  };
+
+  const handleButtonClick = () => {
+    void doAction()
+  };
 
   return (
     <div className="p-2">
@@ -24,6 +36,8 @@ function Index() {
         <>
           <Authenticated>
             <div>Authenticated</div>
+            <button onClick={handleButtonClick}>Create model</button>
+            <div>Total models created: {modelCount}</div>
           </Authenticated>
           <Unauthenticated>
             <SignInForm />
@@ -31,5 +45,5 @@ function Index() {
         </>
       </Layout>
     </div>
-  )
+  );
 }
